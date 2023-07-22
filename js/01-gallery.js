cf88cce67e5488528271363441b03ca81a6ee4cf
Your Name <you@example.com>
2023-07-22 20:29:20 +0300

    listener-task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+
+function makeGallery() {
+    const galleryEl = galleryItems.map(item => ` <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>
+    </li>`).join("")
+
+    
+    gallery.innerHTML = galleryEl;
+}
+
+makeGallery()
+
+gallery.addEventListener('click', onModalClick)
+
+function onModalClick(evt) {
+    evt.preventDefault()
+
+    
+}
+
+
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
